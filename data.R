10872a30e79a79d27c5c54cb1b82d11a2abb66b6
colin millar <colinpmillar@gmail.com>
2018-04-04 21:43:14 +0200

    annotations code complete

--- data.R
+++ data.R
@@ -14,7 +14,7 @@ mkdir("data")
 
 # get utility functions
 # I think this should be a smartdotsReport package...
-source("utilities-smartdots.R")
+source("utilities_data.R")
 
 # load configuration
 config <- read_json("config.json", simplifyVector = TRUE)
@@ -24,45 +24,54 @@ config <- read_json("config.json", simplifyVector = TRUE)
 # DB settings
 dbConnection <- 'Driver={SQL Server};Server=SQL06;Database=SmartDots;Trusted_Connection=yes'
 
-# data: one row per set of dots
+# create filter for events
 filter <- paste(sprintf("EventID = %i", config$event_ids), collapse = " or ")
-sqlq <- sprintf(paste("select * FROM vw_report_Annotations where %s"), filter)
 
-# connect to DB and fetch
+# data: one row per set of dots
 msg("downloading annotations for ... ")
+sqlq <- sprintf(paste("select * FROM vw_report_Annotations where %s"), filter)
 conn <- odbcDriverConnect(connection = dbConnection)
 ad <- sqlQuery(conn, sqlq)
 odbcClose(conn)
 
-ad[ad$sample == "Npout_054_whole",]
+# prepare data
+ad <- ad %>%
+      mutate(month = lubridate::month(catch_date),
+             qtr = lubridate::quarter(catch_date),
+             year = lubridate::year(catch_date))
 
+# Calculate modal ages and cv of modal age
+ad_long <- add_modalage(ad, config$ma_method)
+ad_long_ex <- add_modalage(ad[ad$expertise == 1, ], config$ma_method)
 
-# dist: one row per dot
-# reader1, sample, reader_number, mark, distance, ices_area, reader
-# R01, 6698256.jpg, 1, 1, 0, IIIaS, R01 GBR
-# set up sql command
-filter <- "tblEventID = 74 or tblEventID = 77"
-sqlq <- sprintf(paste("select * FROM vw_report_DotsDistances where %s"), filter)
+# Create wide data with one row per sample.
+# Reader will be used as new column headings for the age readings.
+var_in <- c("sample", "length", "sex", "catch_date", "ices_area",
+             "year", "qtr","month","reader", "age", "cv", "modal_age")
 
-# connect to DB and fetch
-msg("downloading dots for ... ")
-conn <- odbcDriverConnect(connection = dbConnection)
-dist <- sqlQuery(conn, sqlq)
-odbcClose(conn)
+ad_wide <- spread(ad_long[var_in], key = reader, value = age)
+ad_wide_ex <- spread(ad_long_ex[var_in], key = reader, value = age)
 
-# Calculate modal ages and create main data structures ########################
-
-# long format with one reader column
-ad_long <- make_data1(ad, "All", "Mode")
-ad_long_ex <- make_data1(ad, "Expert", "Mode")
-
-# wide format with one column per reader
-ad_wide <- make_data2(ad_long)
-ad_wide_ex <- make_data2(ad_long_ex)
 
 # write out input data tables
-write.taf(dist, "data/dist.csv")
+write.taf(ad, "data/data.csv")
 write.taf(ad_long, "data/ad_long.csv")
 write.taf(ad_long_ex, "data/ad_long_ex.csv")
 write.taf(ad_wide, "data/ad_wide.csv")
 write.taf(ad_wide_ex, "data/ad_wide_ex.csv")
+
+
+
+
+# dist: one row per dot
+msg("downloading dots for ... ")
+sqlq <- sprintf(paste("select * FROM vw_report_DotsDistances where %s"), filter)
+conn <- odbcDriverConnect(connection = dbConnection)
+dist <- sqlQuery(conn, sqlq)
+odbcClose(conn)
+
+# calculate distances between dots
+
+
+# write out input data tables
+write.taf(dist, "data/dist.csv")
