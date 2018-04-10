fac4d3d08507b7c7f5155ee652c2068da2cab8cf
Colin Millar <colinpmillar@gmail.com>
2018-04-10 11:50:14 +0200

    patched versions of new functions

--- utilities-patched.R
+++ utilities-patched.R
@@ -1,3 +1,129 @@
+
+age_er_matrix_stock <- function(dat_in){
+
+  # Pre-set list to add AEMs for each ices area
+  list_aem <-list()
+
+  stocks <- unique(dat_in$stock)
+  for (i in stocks) {
+
+  # Relative contribution of each age per modal age (long format)
+  calc_rel_age <- ddply(dat_in[complete.cases(dat_in$age) &
+                                 dat_in$stock==i,],
+                        .(modal_age, age), summarize,
+                        age_per_modal = sum(!is.na(age)))   %>%
+                  merge(., ddply(., .(modal_age), summarize,
+                        tot_per_modal = sum(age_per_modal) ) ) %>%
+                  mutate(rel_age = age_per_modal/tot_per_modal)
+
+  # Matrix on relative contributions of each age per modal age (wide format)
+  ages_err1 <-  spread(calc_rel_age[c("age", "modal_age", "rel_age")],
+                       age, rel_age) %>% setup_nice2(., "modal_age-age")
+
+  # Transpose matrix such that each column sum to 1
+  ages_trans <- setNames(as.data.frame(t(ages_err1[, -1])),
+                         unlist(ages_err1[, 1])) %>%
+                cbind("modal_age" = row.names(.), .)
+
+
+  # Last corrections and add data frame to list
+  ages_trans$modal_age <- as.integer(as.character(ages_trans$modal_age))
+  ages_trans2 <- setup_nice2(ages_trans, "modal_age-age") %>%
+                 mutate(modal_age = paste("Age", modal_age))
+  names(ages_trans2)[names(ages_trans2) %in% c("modal_age")] <- c("Modal age")
+  list_aem[[i]] <- ages_trans2
+
+  }
+
+  return(list(list_aem, areas))
+
+}
+
+get_ages <- function(dat_in){
+
+   cols <- which(names(dat_in) == "modal_age"):ncol(dat_in)
+   dat_out <- dplyr::select(dat_in, cols)
+  return(dat_out)
+}
+
+
+# Table 1 #####################################################################
+
+# Creates Table 1 in GE with sample info, age readings, modal age, CV and PA
+# Additionally APE is also added to the output (not part of GE table 1)
+data_ov <- function(dat_in, dat_in2, event_id, report_token){
+
+
+  # Select only columns of age readings
+  readings <- get_ages(dat_in)
+
+  # Calculate percentage agreement
+  dat_in$perc_agree <- round2(rowSums(readings[, -1] == readings$modal_age,
+                                      na.rm = TRUE)/
+                                      rowSums(!is.na(readings[, -1]))*100)
+
+  # Combine data in correct order
+
+  sample_vars <- select(dat_in, c(sample, length, sex, catch_date,
+                                  ices_area, stock))
+
+  dat_out <- as.data.frame(cbind(sample_vars,
+                                 select(readings, -modal_age),
+                                 select(dat_in, c(modal_age, perc_agree, cv))))
+  #Add APE
+  ape_tab <- get_ape(dat_in2)
+  dat_out2 <- merge(dat_out, ape_tab)
+
+  # Summary information on the age readings
+  dat_sum <- transpose(data.frame(colSums(!is.na(readings[-1]), na.rm = T),
+                                  colSums(is.na(readings[-1]), na.rm = T) ))
+  colnames(dat_sum) <- colnames(readings[-1])
+  dat_sum_out <- cbind("Reader" = c("Total number read",
+                                    "Total number NOT read"), dat_sum)
+
+  # Summary information on the CV and PA
+  sum_stat <- data.frame("Mean PA %" = round2(meanNA(dat_out2$perc_agree)),
+                         "Mean CV %" = round2(meanNA(dat_out2$cv)),
+                         "Mean APE %" = round2(meanNA(dat_out2$ape)))
+  colnames(sum_stat) <- c("Mean PA %", "Mean CV %", "Mean APE %")
+
+  # Renaming selected columns
+  date_vals <- "catch_date"
+  date_name <- "Catch date"
+
+  names(dat_out2)[names(dat_out2) %in%
+                   c("sample", "length", "sex", date_vals, "ices_area", "stock",
+                     "modal_age", "perc_agree",'cv', 'ape')] <-
+                   c("Sample", "Length (mm)", "Sex", date_name,"ICES area",
+                     "ICES stock", "Modal age", "PA %", "CV %", "APE %")
+
+    # form hyper link for tables
+  link_template <-
+    sprintf("[%s](http://smartdots.ices.dk/viewImage?tblEventID=%i&SmartImageID=%s&token=%s)",
+            "%1$i", event_id, "%1$i", report_token)
+  samples <- strsplit(paste(dat_out2$Sample), "-")
+  dat_out2$Sample <-
+    sapply(samples,
+           function(i)
+             paste(
+               sprintf(link_template, as.numeric(i)),
+               collapse = "-")
+    )
+
+  return(list(dat_out2, sum_stat, dat_sum_out))
+
+}
+
+
+
+
+
+
+
+
+
+
+
 get_cv <- function(mean_dat, std_dat, n_read, dat_in){
 
   # CV, (ratio std/mean)
@@ -35,15 +161,6 @@ get_cv <- function(mean_dat, std_dat, n_read, dat_in){
   names(cv_out)[names(cv_out) %in% "modal_age"] <- "Modal age"
 
   return(list(cv_out,cbind("modal_age" = 0:max, "cv" = cv$all)))
-}
-
-
-
-get_ages <- function(dat_in){
-
-   cols <- which(names(dat_in) == "modal_age"):ncol(dat_in)
-   dat_out <- dplyr::select(dat_in, cols)
-  return(dat_out)
 }
 
 
@@ -101,3 +218,4 @@ plot_growth <- function(dist, compl, part,exp) {
 
   p
 }
+
