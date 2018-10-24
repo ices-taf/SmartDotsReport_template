d1e78863b4f27074d26bcb913fa1617764d7e414
colin millar <colinpmillar@gmail.com>
2018-10-24 13:44:05 +0200

    adding messages to the user

--- data.R
+++ data.R
@@ -26,18 +26,33 @@ config <- read_json("config.json", simplifyVector = TRUE)
 ad <- read.taf("bootstrap/data.csv")
 dist <- read.taf("bootstrap/dist.csv")
 
-# quick hacks -------------------------------
+# some messages to the user ------
+
+frmt_vector <- function(x) {
+  paste(paste(names(x), ":", x), collapse = ", ")
+}
+
+msg("Summary of ad (", nrow(ad), " annotations in total):\n",
+    "\t\t approved: ", sum(ad$IsApproved == "True"), ", unapproved: ", sum(ad$IsApproved == "False"), "\n",
+    "\t\t samples with no area: ", sum(ad$ices_area == ""), "\n",
+    "\t\t prep_method: ", frmt_vector(table(ad$prep_method)), "\n")
+
+msg("Summary of dist (", nrow(dist), " dots in total):\n",
+    "\t\t approved: ", sum(dist$IsApproved == "True"), ", unapproved: ", sum(dist$IsApproved == "False"), "\n",
+    "\t\t dots with no area: ", sum(dist$ices_area == ""), "\n",
+    "\t\t dots with distance: ", sum(is.na(dist$pixelsPerMillimeter)), "\n")
+
+
+# prepare data -------------------------------
 
 # keep only approved annotations
 if (config$onlyApproved) {
-  ad <- ad[ad$IsApproved == TRUE, ]
-  dist <- dist[dist$IsApproved == TRUE, ]
+  ad <- ad[ad$IsApproved == "True", ]
+  dist <- dist[dist$IsApproved == "True", ]
 }
 
 # convert reader expertise
 ad$expertise <- c("Basic", "Advanced")[ad$expertise + 1]
-
-# prepare data -------------------------------
 
 # add date columns
 ad <-
