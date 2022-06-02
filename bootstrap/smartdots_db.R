622d57b40d5c720262dc6cd1913935fcfd275d9e
Colin Millar <colin.millar@ices.dk>
2022-06-02 16:17:44 +0200

    small changes to boot script for smartdots data

--- bootstrap/smartdots_db.R
+++ bootstrap/smartdots_db.R
@@ -9,7 +9,22 @@ library(icesTAF)
 library(jsonlite)
 
 # # load configuration
-config <- read_json("initial/data/config.json", simplifyVector = TRUE)
+config <- read_json(taf.data.path("config.json"), simplifyVector = TRUE)
+
+# Download the reader´s expertise data in case the event organizer choose "multistage" approach to estimate the modal age. Download the data,  create and save a csv file.
+if (config$mode_definition == "multistage") {
+  # download the information regarding the expertise of the readers
+
+  expurl <-
+    paste0(
+      "https://smartdots.ices.dk/api/Reporting/getReportingVariablesRandom?",
+      "tblEventID=", config$event_id
+    )
+
+  expdat <- fromJSON(expurl)
+
+  write.taf(expdat, "expdat.csv", quote = TRUE)
+}
 
 # get data from api --------
 zipfile <- "smartdots_data.zip"
@@ -42,27 +57,3 @@ ad <- ad[,names(ad) != "Comment"]
 # write out 'bootstrap' data tables
 write.taf(dist, "dist.csv", quote = TRUE)
 write.taf(ad, "data.csv", quote = TRUE)
-
-
-# Download the reader´s expertise data in case the event organizer choose "multistage" approach to estimate the modal age. Download the data,  create and save a csv file.
-if(config$mode_definition=="multistage") {
-  # Next download the information regarding the expertise of the readers
-  expdatjson <- fromJSON(file=paste0("https://smartdots.ices.dk/api/Reporting/getReportingVariablesRandom?tblEventID=", config$event_id))
-  
-  nrows <- length(expdatjson)
-  namecols <- names(unlist(expdatjson[[1]]))
-  ncols <- length(namecols)
-  
-  expdat <- matrix(NA, ncol=ncols, nrow=nrows)
-  colnames(expdat) <- namecols
-  
-  for(i in 1:nrows)
-  {
-    expdat[i,]= unlist(expdatjson[i])  
-  }
-  
-  write.csv(expdat, "expdat.csv", row.names = FALSE)
-  
-}
-
-
